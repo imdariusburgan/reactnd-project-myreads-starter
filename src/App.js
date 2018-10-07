57424d3e60da18b9c2bb85ec63d1569338716645
Darius Burgan <darius@dariusburgan.com>
2018-10-07 01:04:24 -0400

    Update state when book changes shelves instead of recalling server

--- src/App.js
+++ src/App.js
@@ -25,7 +25,37 @@ export default class BooksApp extends React.Component {
   // This function will update the book's shelf
   updateBookInfo = (book, shelf) => {
     BooksAPI.update(book, shelf).then(() => {
-      this.getAllBooks();
+      // On previous versions, I updated the books by re-calling the API after updating it.
+      // While it looks instantaneous, it can be slow depending on internet speed.
+      // Now, instead of re-calling the server, I'm updating the state manually.
+      this.setState(prevState => {
+        // This variable will let me know if the book we're updating is currently shown on our shelves. We set this to false as a starting point.
+        let bookAlreadyExist = false;
+
+        // This function will map through each book in our state and
+        // compare that books's ID with the ID of the book we're updating.
+        // If both book ID's match, that means the book is already on one of our shelves.
+        // So, we update the shelf of that book and turn our BookAlreadyExist variable to true.
+        // We return each book at the end in order to build an array.
+        let stateUpdate = prevState.Books.map(b => {
+          if (b.id === book.id) {
+            b.shelf = shelf;
+            bookAlreadyExist = true;
+          }
+          return b;
+        });
+
+        // This function will check if our BookAlreadyExist variable is false.
+        // If so, the book we're updating will be updated and pushed to the stateUpdate variable
+        // If the variable is set to true, this conditional statement will not run.
+        if (!bookAlreadyExist) {
+          book.shelf = shelf;
+          stateUpdate.push(book);
+        }
+        // Now, we update the 'Books' state with the array we were buildng via the 'stateUpdate' variable
+        return { Books: stateUpdate };
+      });
+      // this.getAllBooks();
     });
   };
 
